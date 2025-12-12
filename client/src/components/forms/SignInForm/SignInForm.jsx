7f29a9677a8a03226cfe0c972f1d1106ea7697e5
Olga Lashuk <olga.lashuck@gmail.com>
2025-12-13 00:01:06 +0100

    Make Header and start Sign in form

---
+++ client/src/components/forms/SignInForm/SignInForm.jsx
@@ -0,0 +1,62 @@
+import { useFormik } from "formik";
+import { useNavigate } from "react-router-dom";
+import * as Yup from 'yup';
+
+
+const SignInScheme = Yup.object({
+    login: Yup.string().required('Required'),
+    password: Yup.string().required('Required'),
+})
+
+
+export default function SignInForm() {
+    const navigate = useNavigate();
+
+    const formik = useFormik({
+        initialValues: {
+            login: '',
+            password: '',
+        },
+        validationSchema: SignInScheme,
+        onSubmit: values => {
+            //todo: ckeck user's login and password in the db 
+            navigate('/');
+        },
+    })
+
+    return (
+        <form className="signin-form" onSubmit={formik.handleSubmit}>
+            <div>
+                {formik.errors.login && <div className='error-message'>{formik.errors.login}</div>}
+                <label htmlFor="login">Login</label>
+                <input
+                    type="text"
+                    name="login"
+                    placeholder="Enter login"
+                    value={formik.values.login}
+                    onChange={formik.handleChange}
+                />
+            </div>
+            
+            <div>
+                {formik.errors.password && <div className='error-message'>{formik.errors.password}</div>}
+                <label htmlFor="password">Password</label>
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Enter password"
+                    value={formik.values.password}
+                    onChange={formik.handleChange}
+                />
+            </div>
+
+            <div>
+                <label>Are you not registered yet?</label>
+                <a href="/signup">Sign up</a>
+            </div>
+
+            <button type="submit">Sign in</button>
+            <button type="button" onClick={() => navigate('/')}>Cancel</button>
+        </form>
+    )
+}
--- client/src/components/layout/Header/Header.jsx
+++ client/src/components/layout/Header/Header.jsx
@@ -1,15 +1,40 @@
-import React from 'react';
+import { useNavigate } from "react-router-dom";
+import logo from '../../../assets/logo.png';
+
+
 
 function Header() {
+    const navigate = useNavigate();
+
+    const handleSignIn = () => {
+        navigate('/signin');
+    }
+
+    const handleSignUp = () => {
+        navigate('/signup');
+    }
+
     return (
-        <div>
-            <nav>
+        <header className="header">
+            <div className="header-left">
+                <a href="/">
+                    <img src={logo} alt="logo" className="header-left-logo" />
+                    <span className="header-left-title">AutoMaster</span>
+                </a>
+            </div>
+
+            <nav className="header-menu">
                 <a href="/orders">Orders</a>
                 <a href="/test-drives">Test Drives</a>
                 <a href="/cars">Catalog</a>
                 <a href="/">Main</a>
             </nav>
-        </div>
+
+            <div className="header-signs">
+                <button type="button" onClick={handleSignIn} className="header-signs-btn">Sign in</button>
+                <button type="button" onClick={handleSignUp} className="header-signs-btn">Sign up</button>
+            </div>
+        </header>
     );
 }
 
